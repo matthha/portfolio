18982999f622a52887cf897fdd1fdb4bece7c445
matthha <122128508+matthha@users.noreply.github.com>
2024-02-08 16:58:26 +0000

    Update Home

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,11 @@ const Header = () => {
 
     return (
         <div className="grid">
-        <img width="198" height="36" src="https://nexus.engin.umich.edu/wp-content/uploads/2022/10/nexus-ME-logo-formal.svg" className="custom-logo astra-logo-svg entered lazyloaded" alt="Nexus at Michigan Engineering" decoding="async" data-lazy-src="https://nexus.engin.umich.edu/wp-content/uploads/2022/10/nexus-ME-logo-formal.svg" data-ll-status="loaded"></img>
+        <div className="splash"><h1>Matthew Hall</h1></div>
+        {/* <img width="198" height="36" src="https://nexus.engin.umich.edu/wp-content/uploads/2022/10/nexus-ME-logo-formal.svg" className="custom-logo astra-logo-svg entered lazyloaded" alt="Nexus at Michigan Engineering" decoding="async" data-lazy-src="https://nexus.engin.umich.edu/wp-content/uploads/2022/10/nexus-ME-logo-formal.svg" data-ll-status="loaded"></img> */}
         <div className="grid g4">
-            <div className='b'>Contact</div>
-            <div className='b'>Give</div>
+            <div className=''></div>
+            <div className=''></div>
             <div className='b'>News</div>
             <div className='b'>Search</div>
         </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ const Nav = ( {setScreen} ) => {
     return (
         <div className="nav grid g5">
         
-        <h1 className='b' >About Us</h1>
+        <h1 className='b' onClick={(e) => setScreen('Basic')}>Home</h1>
         <h1 className='b' onClick={(e) => setScreen('Learn')}>Learn</h1>
         <h1 className='b'>Teach</h1>
         <h1 className='b'>Partner</h1>
